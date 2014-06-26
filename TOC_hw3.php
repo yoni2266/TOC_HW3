3afeb4d6e562b4752c8195721f03d4315bfd4cce
Ting-Yu Lin <yoni2266@hotmail.com>
2014-06-26 11:29:34 +0800

    TOC_hw3.php
    1. run this process in order to execute hw3.php on the host server.

---
+++ TOC_hw3.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<meta 
+	http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<?php 
+	echo "<form name=\"form\" method=\"post\" action=\"hw3.php\">"; 
+	echo "URL : <input type=\"text\" name=\"url\" /> <br>"; 
+	echo "鄉鎮市區 : <input type=\"text\" name=\"county\" /> <br>"; 
+	echo "土地區段位置或建物區門牌 : <input type=\"text\" name=\"location\" /> <br>"; 
+	echo "交易年 : <input type=\"text\" name=\"year\" /> <br>"; 
+	echo "<input type=\"submit\" name=\"button\" value=\"GO!\" />"; 
+	echo "</form>"; 
+?>
+</body>
+</html>
